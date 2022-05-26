7b94777ed9373649ba9e634a8104a6b88f3f123a
tang <2856440943@qq.com>
2022-05-26 20:41:27 +0800

    增加获取所有对象功能

--- src/main/java/com/mybatisplus/controller/UserController.java
+++ src/main/java/com/mybatisplus/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -13,6 +14,10 @@ public class UserController {
     @Resource
     private UserService service;
 
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return service.list();
+    }
 
     @GetMapping("/user/{id}")
     public User getUserById(@PathVariable("id") Integer id) {
