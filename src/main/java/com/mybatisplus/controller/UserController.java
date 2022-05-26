1c6725abd637ea62aaf9d371c8404e67a6d469d0
tang <2856440943@qq.com>
2022-05-26 15:12:16 +0800

    first commit

--- src/main/java/com/mybatisplus/controller/UserController.java
+++ src/main/java/com/mybatisplus/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.mybatisplus.controller;
 
+import com.mybatisplus.entities.User;
 import com.mybatisplus.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
@@ -12,7 +14,12 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/user")
-    private Long getAllUser() {
+    public Long getAllUser() {
         return service.count();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        return service.getById(id);
     }
 }
